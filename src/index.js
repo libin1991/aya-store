aee0c1ce5fc75a54efa1af17a53fcdf05bf615ff
milobluebell <1444828173@qq.com>
2019-04-01 22:39:19 +0800

    发布0.0.4

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 /**
- * ▶ ayaStore 0.1.0  
+ * ▶ ayaStore 0.0.4
  * <https://github.com/milobluebell/aya-store>
  * 
  * copyright 2019 milobluebell
  * available under MIT license
  * 
  * @author milobluebell
- * @version 0.1.0
+ * @version 0.0.4
  */
 
 import StoreProto from './StoreProto';
